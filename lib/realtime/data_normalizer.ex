87a61f14a58f825689cf99e8594e5496e15fbd18
Juha Lehtonen <juha.lehtonen@relexsolutions.com>
2024-03-16 21:49:35 +0200

    00:32:00 - Document DataNormalizer.normalize/2

--- lib/realtime/data_normalizer.ex
+++ lib/realtime/data_normalizer.ex
@@ -6,6 +6,23 @@ defmodule Realtime.DataNormalizer do
 
   alias Ecto.Changeset
 
+  @doc """
+  Given loosely typed `data` and a `schema`, normalizes the types of `data` to match given
+  `schema`. Discards keys in `data` not present in `schema`, and can optionally validate
+  that `required` keys are present in `data`.
+
+  Returns `{:ok, normalized_params}` if data has been correctly normalized, or
+  `{:error, changeset}` if there were errors in data normalization.
+
+  ## Examples
+
+      iex> Realtime.DataNormalizer.normalize(
+      iex>   %{"id" => "123", "date" => "2024-03-16"},
+      iex>   [id: :integer, date: :date]
+      iex> )
+      {:ok, %{id: 123, date: ~D[2024-06-16]}}
+
+  """
   def normalize(data, schema, required \\ [])
       when is_map(data) and is_list(schema) and is_list(required) do
     types = get_types_from_schema(schema)
